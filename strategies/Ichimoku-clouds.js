43a3c7746a260172abba8372575132ddc6bdf04f
Paolo Oranges <paolo.oranges@gmail.com>
2018-08-21 19:53:34 +0200

    start implementation of Ichimoku-clouds strategy

--- strategies/Ichimoku-clouds.js
+++ strategies/Ichimoku-clouds.js
@@ -1,14 +1,14 @@
-var log = require('../core/log');
+let log = require('../core/log');
 const IchimokuCloud = require('./indicators/ICHIMOKU-CLOUD.js');
 
 // Let's create our own strat
-var strat = {};
+let strat = {};
 
 // Prepare everything our method needs
 strat.init = function() {
   this.name = 'Ichimoku Clouds';
 
-  this.addIndicator('IchimokuCloud', 'ICHIMOKU-CLOUD', this.settings);
+  this.addIndicator('ichimokuCloud', 'ICHIMOKU-CLOUD', this.settings);
 }
 
 // What happens on every new candle?
@@ -23,7 +23,14 @@ strat.log = function() {
 // Based on the newly calculated
 // information, check if we should
 // update or not.
-strat.check = function() {
+strat.check = function(candle) {
+  let price = candle.close;
+  let ichimokuCloud = this.indicators.ichimokuCloud;
+  
+  let cloudColor = ichimokuCloud.getCloudColor();
+  let trend = ichimokuCloud.getTrend();
+
+  log.debug('Cloud Color: ', cloudColor, ' trend: ', trend);
 
 }
 
