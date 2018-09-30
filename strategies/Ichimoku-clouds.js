98dd3c465f1f230d9105ac06422000ac6e425bf9
Paolo Oranges <paolo.oranges@gmail.com>
2018-09-30 18:01:17 +0200

    start implementing strategy on ichimoku

--- strategies/Ichimoku-clouds.js
+++ strategies/Ichimoku-clouds.js
@@ -1,5 +1,8 @@
 let log = require('../core/log');
 const IchimokuCloud = require('./indicators/ICHIMOKU-CLOUD.js');
+const IchimokuCloudTypes = require('./indicators/ICHIMOKU-CLOUD-TYPES.js');
+const TREND = IchimokuCloudTypes.TREND;
+const CLOUD_COLOR = IchimokuCloudTypes.CLOUD_COLOR;
 
 // Let's create our own strat
 let strat = {};
@@ -7,6 +10,9 @@ let strat = {};
 // Prepare everything our method needs
 strat.init = function() {
   this.name = 'Ichimoku Clouds';
+  this.bought = false;
+  this.lastPrice = 0;
+  this.lastTrend = TREND.FLAT;
 
   this.addIndicator('ichimokuCloud', 'ICHIMOKU-CLOUD', this.settings);
 }
@@ -25,13 +31,66 @@ strat.log = function() {
 // update or not.
 strat.check = function(candle) {
   let price = candle.close;
-  let ichimokuCloud = this.indicators.ichimokuCloud;
   
-  let cloudColor = ichimokuCloud.getCloudColor();
-  let trend = ichimokuCloud.getTrend();
+  if(this.bought)
+  {
+    this.checkSell(price);
+  }
+  else
+  {
+    this.checkBuy(price);
+  }
 
-  console.debug('Cloud Color: ', cloudColor, ' trend: ', trend);
+}
 
+strat.checkSell = function(price)
+{
+  
+}
+
+strat.checkBuy = function(price)
+{
+  
+  const ichimokuCloud = this.indicators.ichimokuCloud;
+  const trend = ichimokuCloud.getTrend();
+  const color = ichimokuCloud.getCloudColor();
+  const result = ichimokuCloud.result;
+
+  if(trend === TREND.UP)
+  {
+    if(color === CLOUD_COLOR.GREEN)
+    {
+      if(result.tenkanSen > result.kijunSen) // conversion line > base line
+      {
+        this.adviceBuy();
+      }
+    }
+    else
+    {
+      if (color === CLOUD_COLOR.GREEN) 
+      {
+        if (result.tenkanSen > result.kijunSen) // conversion line > base line
+        {
+          this.adviceBuy();
+        }
+      }
+      else if(trend == TREND.FLAT)
+      {
+        if (result.tenkanSen > result.kijunSen && this.lastTrend === TREND.DOWN) // conversion line > base line
+        {
+          this.adviceBuy();
+        }
+      }
+    }
+  }
+
+  this.lastTrend = trend;
+}
+
+strat.adviceBuy = function()
+{
+  console.debug("Advise Buy");
+  this.bought = true;
 }
 
 module.exports = strat;
